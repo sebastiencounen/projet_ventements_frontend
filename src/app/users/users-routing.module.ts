2986b993824587459d42d53f0a96fa61d06ab088
Sébastien Counen <sebastien.counen@outlook.com>
2020-12-06 18:03:15 +0100

    Added sign up route and component declaration in user routing module

--- src/app/users/users-routing.module.ts
+++ src/app/users/users-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {SignInFormComponent} from './components/sign-in-form/sign-in-form.component';
+import {SignUpFormComponent} from './components/sign-up-form/sign-up-form.component';
 
 const routes: Routes = [
-  { path: 'users/sign-in', component: SignInFormComponent }
+  { path: 'users/sign-in', component: SignInFormComponent },
+  { path: 'users/sign-up', component: SignUpFormComponent }
 ];
 
 @NgModule({
@@ -12,6 +14,7 @@ const routes: Routes = [
 })
 export class UsersRoutingModule {
   static components = [
-    SignInFormComponent
+    SignInFormComponent,
+    SignUpFormComponent
   ];
 }
