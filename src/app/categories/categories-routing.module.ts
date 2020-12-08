07c5c99e08267fc0bbb878885d5f1ac8693324e2
Lorenzo <la188504@student.helha.be>
2020-12-08 09:56:16 +0100

    Removing list-subcategories and manage-subcategories components

--- src/app/categories/categories-routing.module.ts
+++ src/app/categories/categories-routing.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {ManageCategoriesComponent} from './components/manage-categories/manage-categories.component';
 import {ListCategoriesComponent} from './components/list-categories/list-categories.component';
-import {ManageSubcategoriesComponent} from './components/manage-subcategories/manage-subcategories.component';
-import {ListSubcategoriesComponent} from './components/list-subcategories/list-subcategories.component';
 import {ManageItemsComponent} from '../items/components/manage-items/manage-items.component';
 
 const routes: Routes = [
@@ -22,8 +20,6 @@ const routes: Routes = [
 export class CategoriesRoutingModule {
   static components = [
     ManageCategoriesComponent,
-    ListCategoriesComponent,
-    ListSubcategoriesComponent,
-    ManageSubcategoriesComponent
+    ListCategoriesComponent
   ];
 }
