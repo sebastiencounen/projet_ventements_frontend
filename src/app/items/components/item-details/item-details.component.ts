03561b66e82e06a42c83f9b0424fbddd8e5854f3
Lorenzo <la188504@student.helha.be>
2020-12-08 11:02:10 +0100

    Display of item image in item-details

---
+++ src/app/items/components/item-details/item-details.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import {Item} from '../../types/item';
+import {ItemsServerService} from '../../repositories/items-server.service';
+import {ActivatedRoute} from '@angular/router';
+import {filter, map, switchMap} from 'rxjs/operators';
+
+@Component({
+  selector: 'app-item-details',
+  templateUrl: './item-details.component.html',
+  styleUrls: ['./item-details.component.scss']
+})
+export class ItemDetailsComponent implements OnInit {
+
+  item: Item;
+
+  constructor(private itemService: ItemsServerService,
+              private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.getItemById();
+  }
+
+  private getItemById() {
+    this.route.paramMap.pipe(
+      filter(params => +params.get('id') !== 0),
+      map(params => +params.get('id')),
+      switchMap(id => this.itemService.getItemById(id))
+    ).subscribe(item => this.item = item);
+  }
+}
