a2b326d4f5fc1c53ca915d6ca464598775ac5346
Sébastien Counen <sebastien.counen@outlook.com>
2020-12-10 22:33:26 +0100

    Added wishlist module + wishlist type

---
+++ src/app/wishlist/types/wishlist.ts
@@ -0,0 +1,9 @@
+import {User} from '../../users/types/user';
+import {Item} from '../../items/types/item';
+
+export interface Wishlist {
+  id?: number;
+  addedAt?: Date;
+  userWishList: User;
+  itemWishList: Item;
+}
