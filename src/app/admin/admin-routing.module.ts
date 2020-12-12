ccaf87ff25614c3dc78da0a6779d041f34e25e12
Sébastien Counen <sebastien.counen@outlook.com>
2020-12-12 11:23:47 +0100

    Added item-form component + child route from dashboard

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {AdminGuardService} from './auth/admin-guard.service';
 import {AdminDashboardComponent} from './components/admin-dashboard/admin-dashboard.component';
+import {ItemFormComponent} from './components/item-form/item-form.component';
 
 const routes: Routes = [
-  { path: 'admin/dashboard', canActivate: [AdminGuardService], component: AdminDashboardComponent }
+  { path: 'admin/dashboard', canActivate: [AdminGuardService], component: AdminDashboardComponent,
+    children: [
+      { path: 'items/add', component: ItemFormComponent }
+    ]
+  }
 ];
 
 @NgModule({
@@ -14,5 +19,6 @@ const routes: Routes = [
 export class AdminRoutingModule {
   static components = [
     AdminDashboardComponent,
+    ItemFormComponent
   ];
 }
